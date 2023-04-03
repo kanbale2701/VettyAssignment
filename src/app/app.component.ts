7949b6e1323263e94e7c75dfd217c10b93905363
kanbale2701 <110616607+kanbale2701@users.noreply.github.com>
2023-04-03 14:57:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Jeeraproject';
+  title = 'Jira Board (Vetty Assignment)';
+  listName = '';
+  id = 1;
+  cardId = 1;
+  public jiraBoard: any[] = [];
+
+  addList() {
+    if (this.listName) {
+      this.jiraBoard.push(
+        {
+          header: this.listName,
+          id: this.id,
+          cards: []
+        })
+      this.listName = '';
+      this.id = this.id + 1
+    };
+  }
+
+
+  removeList(val: any) {
+    if (val) {
+      let index = this.jiraBoard.findIndex(x => x.id == val);
+      this.jiraBoard.splice(index, 1)
+    }
+  }
+
+  addCard(val: any) {
+    if (val) {
+      let date = new Date()
+      let jiraBoardList = this.jiraBoard.find(x => x.id == val)
+
+      jiraBoardList.cards.push({
+        header: "Header",
+        description: JSON.stringify(val) + JSON.stringify(this.cardId),
+        time: date,
+        parentId: val,
+        id: JSON.stringify(val) + JSON.stringify(this.cardId)
+      })
+    }
+    this.cardId += 1;
+  }
+  removeCard(val: any, parentId: any) {
+    if (val && parentId) {
+      let cardList = this.jiraBoard.find(x => x.id == parentId).cards;
+      let index = cardList.findIndex((x: any) => x.id == val);
+      cardList.splice(index, 1)
+
+    }
+  }
 }
